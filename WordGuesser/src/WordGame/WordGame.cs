d6c15122ba3d515122068594fd93e407d5111f67
JoshGoodman22 <jgoodman2024@chadwickschool.org>
2021-11-11 11:46:18 -0800

    Josh milestone2 changes

--- WordGuesser/src/WordGame/WordGame.cs
+++ WordGuesser/src/WordGame/WordGame.cs
@@ -33,15 +33,16 @@ namespace WordGuesser
             this.incorrectGuesses = 0;
             this.lettersGuessed = new List<char>();
         }
+
         public string CheckGuess(string guess)
         {
-            guess = guess.Trim()toUpper();
+            guess = guess.Trim().ToUpper();
 
             if (guess.Length != 1)
             {
                 return "You must guess a single letter";
             }
-            else if (char.IsLetter(guess[0]) = false)
+            else if (char.IsLetter(guess[0]) == false)
             {
                 return "You can only Guess leters";
             }
@@ -49,53 +50,61 @@ namespace WordGuesser
             {
                 return $"You've already guessed {guess}";
             }
-            if (this.wordToGuess.Contains(guess) = false)
+
+            if (this.fullWord.Contains(guess) == false)
             {
                 this.incorrectGuesses++;
                 this.lettersGuessed.Add(guess[0]);
-                return $"Ouch! No {guesses}s";
+                return $"Ouch! No {guess}s";
             }
-            
 
+            int count;
+            count = this.CountLetter(guess[0]);
 
-            public int CountLetter(char guess)
+            if (count == 1)
             {
-                throw new System.NotImplementedException();
+                return $"There is 1 {guess}";
             }
+            return $"There are {count} {guess}s";
+        }
+        public int CountLetter(char guess)
+        {
+            throw new System.NotImplementedException();
+        }
 
-            public string GetFullWord()
-            {
-                throw new System.NotImplementedException();
-            }
+        public string GetFullWord()
+        {
+            return this.fullWord;
+        }
 
-            public string GetGuessedLetters()
-            {
-                throw new System.NotImplementedException();
-            }
+        public string GetGuessedLetters()
+        {
+            throw new System.NotImplementedException();
+        }
 
-            public int GetGuessLimit()
-            {
-                throw new System.NotImplementedException();
-            }
+        public int GetGuessLimit()
+        {
+            throw new System.NotImplementedException();
+        }
 
-            public int GetIncorrectGuesses()
-            {
-                throw new System.NotImplementedException();
-            }
+        public int GetIncorrectGuesses()
+        {
+            throw new System.NotImplementedException();
+        }
 
-            public string GetWord()
-            {
-                throw new System.NotImplementedException();
-            }
+        public string GetWord()
+        {
+            throw new System.NotImplementedException();
+        }
 
-            public bool IsGameOver()
-            {
-                throw new System.NotImplementedException();
-            }
+        public bool IsGameOver()
+        {
+            throw new System.NotImplementedException();
+        }
 
-            public bool IsGameWon()
-            {
-                throw new System.NotImplementedException();
-            }
+        public bool IsGameWon()
+        {
+            throw new System.NotImplementedException();
         }
     }
+}
